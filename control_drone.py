fbf97707b4f413a2c85e295bf2e67959c877b2ac
Matthew Liu <matthew_liu@berkeley.edu>
2024-07-14 11:34:16 -0700

    streaming

--- control_drone.py
+++ control_drone.py
@@ -1,12 +1,23 @@
+from threading import Thread
 from djitellopy import Tello
+import cv2, math, time
+import os
+
 
 tello = Tello()
-
-tello.connect()
+tello.connect()#
+tello.streamon()
+#print(tello.get_udp_video_address())
+#print(type(tello.get_udp_video_address()))
+#cap = cv2.VideoCapture(tello.get_udp_video_address())
 tello.takeoff()
-
-tello.move_left(100)
-tello.rotate_counter_clockwise(90)
-tello.move_forward(100)
-
-tello.land()
+tello.move_up(80)
+try:
+    while True:
+        img = tello.get_frame_read().frame#
+        cv2.imshow('frame', img)
+        cv2.waitKey(1)#
+except KeyboardInterrupt:
+    exit(1)
+finally:
+    print("fin")
